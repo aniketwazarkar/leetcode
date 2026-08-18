83c800a4ed796f4d9c54f79a47413daaa3ebf5b4
Aniket Wazarkar <57173176+aniketwazarkar@users.noreply.github.com>
2026-08-18 18:28:08 +0530

    [73] [Time Beats: 100.00%] [Memory Beats: 24.43%] - LeetPush

--- 73-set-matrix-zeroes/73-Set-Matrix-Zeroes.cpp
+++ 73-set-matrix-zeroes/73-Set-Matrix-Zeroes.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-
     void setZeroes(vector<vector<int>>& arr) {
         int m = arr.size();
         int n = arr[0].size();
 
-        vector<int> rowMark(m, 0);
-        vector<int> colMark(n, 0);
+        int col0 = 1;
 
+        // Step 1: use first row and first column as markers
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (arr[i][j] == 0) {
-                    rowMark[i] = 1;
-                    colMark[j] = 1;
+                    arr[i][0] = 0;
+                    if (j != 0) {
+                        arr[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rowMark[i] == 1 || colMark[j] == 1) {
-                    arr[i][j] = 0;
+        // Step 2: zero out cells based on markers (skip row 0 and col 0!)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] != 0) {
+                    if (arr[0][j] == 0 || arr[i][0] == 0) {
+                        arr[i][j] = 0;
+                    }
                 }
+            }
+        }
+
+        // Step 3: handle row 0 (length n, not m)
+        if (arr[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                arr[0][j] = 0;
+            }
+        }
+
+        // Step 4: handle column 0 (length m, not n)
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                arr[i][0] = 0;
             }
         }
     }
