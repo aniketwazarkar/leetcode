629cebf94a9ff04c8d05c82c19ecb4b49ec14366
Aniket Wazarkar <57173176+aniketwazarkar@users.noreply.github.com>
2026-08-18 19:54:14 +0530

    [118] [Time Beats: 15.13%] [Memory Beats: 88.96%] - LeetPush

--- 118-pascal's-triangle/118-Pascal's-Triangle.cpp
+++ 118-pascal's-triangle/118-Pascal's-Triangle.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> triangle;
-
-        for (int row = 0; row < numRows; row++) {
-            // Make a new empty row, sized correctly (row+1 numbers)
-            vector<int> currentRow(row + 1);
-
-            // The first and last number in every row is always 1
-            currentRow[0] = 1;
-            currentRow[row] = 1;
-
-            // Fill in the middle numbers
-            for (int col = 1; col < row; col++) {
-                // Add the two numbers above: same column and one column before,
-                // from previous row
-                currentRow[col] =
-                    triangle[row - 1][col - 1] + triangle[row - 1][col];
+        vector<vector<int>> ans;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> v(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                v[j] = ans[i - 1][j - 1] + ans[i - 1][j];
             }
-
-            // Add this finished row to our triangle
-            triangle.push_back(currentRow);
+            ans.push_back(v);
         }
-
-        // Now let's print it nicely
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col <= row; col++) {
-                cout << triangle[row][col] << " ";
-            }
-            cout << endl;
-        }
-        return triangle;
+        return ans;
     }
 };
